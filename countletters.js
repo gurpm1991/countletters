2f9bea32d40ed1f616b0dd3127d6e448b394f3f8
Gurpreet <gurpm91@gmail.com>
2017-10-04 17:30:29 +0000

    code updated

--- countletters.js
+++ countletters.js
@@ -1,13 +1,8 @@
 var input = String(process.argv.slice(2)).split('');
-console.log(input);
+//console.log(input);
 
-var letterCount = {
-	a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, 
-	g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, 
-	m: 0, n: 0, o: 0, p: 0, q: 0, r: 0, 
-	s: 0, t: 0, u: 0, v: 0, w: 0, x: 0, 
-	y: 0, z: 0
-}
+var letterCount = {};
+
 
 
 function countLetters(input, letterCount){
@@ -15,6 +10,8 @@ function countLetters(input, letterCount){
 		var word = input[i].toLowerCase()
 		if (word in letterCount){
 			letterCount[word] += 1
+		} else {
+			letterCount[word] = 1;
 		}
 	}
 	return letterCount
@@ -22,16 +19,9 @@ function countLetters(input, letterCount){
 
 
 
-function deleteZeros (wordCount) {
-  for(key in wordCount) {
-    if (wordCount[key] == 0 ){
-      delete wordCount[key];
-    }
-  }
-  return wordCount
-}
 
+console.log (countLetters (input, letterCount));
 
-console.log (deleteZeros(countLetters (input, letterCount)));
+//console.log(typeof countLetters (input, letterCount));
 
 //console.log(typeof countLetters (input, letterCount));
