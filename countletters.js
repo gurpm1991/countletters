8f0fadaee271c0e3ef2321d4e854b073ac4a6ecc
gurpm1991 <gurpm91@gmail.com>
2017-11-01 16:54:05 +0000

    done assignment

--- countletters.js
+++ countletters.js
@@ -1,27 +1,25 @@
-var input = String(process.argv.slice(2)).split('');
-//console.log(input);
-
+var input = String(process.argv.slice(2));
+var input = input.replace(/\,*\s*/ig, '').split('');
 var letterCount = {};
 
-
-
-function countLetters(input, letterCount){
-	for (var i = 0; i < input.length; i++) {
-		var word = input[i].toLowerCase()
+function countLetters (input, letterCount){
+	for (var i = 0; i < input.length; i++){
+		var word = input[i].toLowerCase();
+	
 		if (word in letterCount){
 			letterCount[word] += 1
 		} else {
 			letterCount[word] = 1;
-		}
+		} 
 	}
-	return letterCount
-}
-
+	return letterCount;
+};
 
 
 
 console.log (countLetters (input, letterCount));
 
-//console.log(typeof countLetters (input, letterCount));
+// console.log(typeof countLetters (input, letterCount));
 
-//console.log(typeof countLetters (input, letterCount));
+// console.log(typeof countLetters (input, letterCount));
+
